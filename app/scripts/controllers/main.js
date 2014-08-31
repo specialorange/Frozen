f775cf2efdeb582d2ea14ca97577495ab33e5ef6
Colby Rogness <colbyrogness@gmail.com>
2014-08-31 02:17:37 -0400

    display the oldest expiration date and the corresponding add date in the view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,20 +10,39 @@
 angular.module('frozenApp')
   .controller('MainCtrl', [ '$scope', 'ItemService', function ($scope, ItemService) {
     ItemService.getItems()
-    	.then(function(data) {
-    		for (var i = 0; i < data.items.length; i++) {
-    			var item = data.items[i];
-    			var quantity = 0;
-    			var oldestExpirationDate, oldestAddedDate;
-    			for (var expiration in item.dates) {
-    				quantity++;
-    				var added = item.dates[expiration];
-    			}
-    			item.quantity = quantity;
-    		};
-    		// for (var item in data.items) {
-    			
-    		// }
-    		$scope.allItems = data.items;
-    	});
+        .then(function(data) {
+            var moment = window.moment;
+
+            // loop through the items
+            for (var i = 0; i < data.items.length; i++) {
+                var item = data.items[i];
+                var quantity = 0;
+                var expiration, added;
+
+                // loop through the expiration dates
+                for (var key in item.dates) {
+                    quantity++;
+
+                    // parse the dates with moment.js
+                    expiration = moment(key, 'MM/DD/YYYY');
+                    added = moment(item.dates[key], 'MM/DD/YYYY');
+
+                    // set the dates to the first if it's not there yet
+                    if (!item.oldestExpirationDate) {
+                        item.oldestExpirationDate = expiration;
+                        item.oldestAddedDate = added;
+                    }
+
+                    if (expiration.isBefore(item.oldestExpirationDate)) {
+                        item.oldestExpirationDate = expiration;
+                        item.oldestAddedDate = added;
+                    }
+                }
+                item.quantity = quantity;
+            }
+            // for (var item in data.items) {
+                
+            // }
+            $scope.allItems = data.items;
+        });
   } ]);
