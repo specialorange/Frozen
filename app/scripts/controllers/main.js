9fe9179b8df94b66abc614849d8e5fcec62883c5
Colby Rogness <colbyrogness@gmail.com>
2014-08-31 00:24:17 -0400

    start trying to display the items

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,3 +8,22 @@
  * Controller of the frozenApp
  */
 angular.module('frozenApp')
+  .controller('MainCtrl', [ '$scope', 'ItemService', function ($scope, ItemService) {
+    ItemService.getItems()
+    	.then(function(data) {
+    		for (var i = 0; i < data.items.length; i++) {
+    			var item = data.items[i];
+    			var quantity = 0;
+    			var oldestExpirationDate, oldestAddedDate;
+    			for (var expiration in item.dates) {
+    				quantity++;
+    				var added = item.dates[expiration];
+    			}
+    			item.quantity = quantity;
+    		};
+    		// for (var item in data.items) {
+    			
+    		// }
+    		$scope.allItems = data.items;
+    	});
+  } ]);
