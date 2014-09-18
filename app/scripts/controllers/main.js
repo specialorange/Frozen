9f023989da076de981bfff621ff2a89c9f482a33
Colby Rogness <colbyrogness@gmail.com>
2014-09-18 16:13:06 -0400

    it loads data from firebase

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/* globals moment */
-
 /**
  * @ngdoc function
  * @name frozenApp.controller:MainCtrl
@@ -10,50 +8,60 @@
  * Controller of the frozenApp
  */
 angular.module('frozenApp')
-  .controller('MainCtrl', [ '$scope', 'ItemService', 'ExpirationMatchingService', function ($scope, ItemService, ExpirationMatchingService) {
-    ItemService.getItems()
-        .then(function(data) {
-            // var moment = window.moment;
+  .controller('MainCtrl', [ '$scope', 'ItemService', function ($scope, ItemService) {
 
-            // loop through the items
-            for (var i = 0; i < data.items.length; i++) {
-                var item = data.items[i];
-                var quantity = 0;
-                var expiration, added;
+    ItemService.onFirstLoad(function (data) {
+        data.$bindTo($scope, 'data');
+    });
 
-                // loop through the expiration dates
-                for (var key in item.dates) {
-                    quantity++;
+    $scope.provider = ItemService;
 
-                    // parse the dates with moment.js
-                    added = moment(key, 'MM/DD/YYYY');
-                    expiration = moment(item.dates[key], 'MM/DD/YYYY');
 
-                    // set the dates to the first if it's not there yet
-                    if (!item.oldestExpirationDate) {
-                        item.oldestExpirationDate = expiration;
-                        item.oldestAddedDate = added;
-                    }
 
-                    if (expiration.isBefore(item.oldestExpirationDate)) {
-                        item.oldestExpirationDate = expiration;
-                        item.oldestAddedDate = added;
-                    }
-                }
 
-                item.quantity = quantity;
-            }
+    // ItemService.getItems()
+    //     .then(function(data) {
+    //         // var moment = window.moment;
+
+    //         // loop through the items
+    //         for (var i = 0; i < data.items.length; i++) {
+    //             var item = data.items[i];
+    //             var quantity = 0;
+    //             var expiration, added;
+
+    //             // loop through the expiration dates
+    //             for (var key in item.dates) {
+    //                 quantity++;
+
+    //                 // parse the dates with moment.js
+    //                 added = moment(key, 'MM/DD/YYYY');
+    //                 expiration = moment(item.dates[key], 'MM/DD/YYYY');
+
+    //                 // set the dates to the first if it's not there yet
+    //                 if (!item.oldestExpirationDate) {
+    //                     item.oldestExpirationDate = expiration;
+    //                     item.oldestAddedDate = added;
+    //                 }
+
+    //                 if (expiration.isBefore(item.oldestExpirationDate)) {
+    //                     item.oldestExpirationDate = expiration;
+    //                     item.oldestAddedDate = added;
+    //                 }
+    //             }
+
+    //             item.quantity = quantity;
+    //         }
             
-            // match the items to ok, expires soon, and expired lists
-            var lists = ExpirationMatchingService.match(data.items);
+    //         // match the items to ok, expires soon, and expired lists
+    //         var lists = ExpirationMatchingService.match(data.items);
 
-            $scope.green = lists.green;
-            $scope.yellow = lists.yellow;
-            $scope.red = lists.red;
+    //         $scope.green = lists.green;
+    //         $scope.yellow = lists.yellow;
+    //         $scope.red = lists.red;
 
-            $scope.unexpired = lists.unexpired;
-            $scope.expired = lists.expired;
+    //         $scope.unexpired = lists.unexpired;
+    //         $scope.expired = lists.expired;
 
-            $scope.allItems = data.items;
-        });
+    //         $scope.allItems = data.items;
+    //     });
     } ]);
--- app/scripts/providers/ItemService.js
+++ app/scripts/providers/ItemService.js
@@ -1,27 +1,62 @@
 'use strict';
 
+/* globals Firebase */
+
 angular.module('frozenApp')
-.factory('ItemService', function($http, $q) {
+.factory('ItemService', ['$firebase', function($firebase) {
 
-     return {
-           getItems: function() {
-                var deferred = $q.defer();
-                $http.get('data/data.json').success(function(data) {
-                    deferred.resolve(data);
-                }).error(function(){
-                    deferred.reject();
-                });
+	// ItemService constructor
+	function ItemService() {
+		this._ref = new Firebase('https://frozenapp.firebaseio.com/');
+		this._sync = $firebase(this._ref);
+		// download the data into a local object
+		// this._syncObject = this._sync.items.$asObject();
+		this._data = this._sync.$asObject();
+		// <DEBUG>
+		window.sync = this._sync;
+		window.data = this._data;
+		// </DEBUG>
+	}
 
-                return deferred.promise;
+	// ItemService methods and fields
+	ItemService.prototype = {
 
-            // This also works
-            //since $http.get returns a promise,
-            //and promise.then() also returns a promise
-            //that resolves to whatever value is returned in it's
-            //callback argument, we can return that.
-            // return $http.get('data/data.json').then(function(result) {
-            //   return result.data;
-            // });
-           }
-     };
-});
+		onFirstLoad: function(callback) {
+			this._data.$loaded().then(callback);
+		},
+
+		getItems: function() {
+			// console.log(this._data.items);
+			return this._data.items;
+		}
+	};
+
+	return new ItemService();
+
+	
+	// synchronize the object with a three-way data binding
+	// click on `index.html` above to see it used in the DOM!
+	// syncObject.$bindTo($scope, 'data');
+
+	 // return {
+	 //       getItems: function() {
+	 //            var deferred = $q.defer();
+	 //            $http.get('data/data.json').success(function(data) {
+	 //                deferred.resolve(data);
+	 //            }).error(function(){
+	 //                deferred.reject();
+	 //            });
+
+	 //            return deferred.promise;
+
+	 //        // This also works
+	 //        //since $http.get returns a promise,
+	 //        //and promise.then() also returns a promise
+	 //        //that resolves to whatever value is returned in it's
+	 //        //callback argument, we can return that.
+	 //        // return $http.get('data/data.json').then(function(result) {
+	 //        //   return result.data;
+	 //        // });
+	 //       }
+	 // };
+}]);
